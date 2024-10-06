1845ffdb629eff5e4a47816a9da0e9316bab958b
Leonid <lporoshi@student.42heilbronn.de>
2024-10-06 08:50:49 +0200

    do ex01

---
+++ ex01/Span.cpp
@@ -0,0 +1,52 @@
+#include "Span.hpp"
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include  <utility>
+#include <iostream>
+
+Span::Span() {}
+
+Span::Span(const Span &other)
+    : storage(other.storage), capacity(other.capacity) {}
+
+Span &Span::operator=(const Span &other) {
+  if (this != &other) {
+    storage = other.storage;
+    capacity = other.capacity;
+  }
+  return *this;
+}
+
+Span::~Span() {}
+
+Span::Span(unsigned int N) : capacity(N) {}
+
+void Span::addNumber(int n) {
+  if (storage.size() >= capacity) {
+    throw std::overflow_error("Span capacity exceeded");
+  }
+  storage.push_back(n);
+}
+
+int Span::longestSpan() {
+  if (storage.size() < 2) {
+    throw std::logic_error("Not enough numbers to find a span.");
+  }
+  std::pair<std::vector<int>::iterator, std::vector<int>::iterator> extremes = std::minmax_element(storage.begin(), storage.end());
+  return *extremes.second - *extremes.first;
+}
+
+int Span::shortestSpan() {
+  if (storage.size() < 2) {
+    throw std::logic_error("Not enough numbers to find a span.");
+  }
+
+  std::vector<int> sorted_storage = storage;
+  std::sort(sorted_storage.begin(), sorted_storage.end());
+
+  std::vector<int> diffs(storage);
+  std::adjacent_difference(sorted_storage.begin(), sorted_storage.end(),
+                           diffs.begin());
+  return *(std::min_element(diffs.begin() + 1, diffs.end()));
+}
---
+++ ex01/Span.hpp
@@ -0,0 +1,43 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <stdexcept>
+
+
+class Span {
+private:
+  Span();
+  Span &operator=(const Span &other);
+
+public:
+  Span(const Span &other);
+  ~Span();
+
+public:
+  Span(unsigned int N);
+  void addNumber(int n);
+  int longestSpan();
+  int shortestSpan();
+
+public:
+  template <typename IterType> void addNumbers(IterType begin, IterType end) {
+    typename std::iterator_traits<IterType>::difference_type append_size =
+        (std::distance(begin, end));
+    if (append_size < 0) {
+      throw std::logic_error("iterators in reversed order");
+      return;
+    }
+    if (append_size + storage.size() > capacity) {
+      throw std::overflow_error("Span capacity exceeded");
+      return;
+    }
+    storage.insert(storage.end(), begin, end);
+  }
+
+private:
+  std::vector<int> storage;
+  unsigned int capacity;
+};
+
+#endif // SPAN_HPP
